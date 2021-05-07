dc23f725a28017a5472092fd3975b687c6301988
eavior <elisha.avior@gmail.com>
2021-05-07 16:31:08 +0300

    v0.6

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ const { postgrator } = require('./lib/db');
 const { uploadedFilesFolderName } = require('./middlewares/multipart');
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(express.json());
 app.use(cors());
 app.use(pino({ level: process.env.LOG_LEVEL }));
